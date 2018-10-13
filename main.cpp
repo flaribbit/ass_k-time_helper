93bc2c824813a9e054c926902c62b592439d3b14
flyzhai <1149761294@qq.com>
2018-10-13 01:38:02 -0500

    Add files via upload
    文件播放功能完成

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "resource.h"
 
+//20181012 1.5小时 界面完成
+//20181013 2小时 音乐播放部分完成
 typedef struct{
 	MCI_OPEN_PARMS open;
 	MCI_PLAY_PARMS play;
@@ -15,19 +17,39 @@ typedef struct{
 struct{
 	HWND timeline;
 	HWND lrclist;
+	HWND time;
+	int duration;
+	char file[MAX_PATH];
+	char lrc[MAX_PATH];
 }ui;
 
 HINSTANCE hInst;
 MCIGLOBAL mci;
-char szFile[MAX_PATH];
+
+void OpenLRC(HWND hwndDlg){
+	OPENFILENAME ofn;
+    ZeroMemory(&ofn,sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.lpstrFile = ui.lrc;//全路径
+    ofn.lpstrFile[0] = 0;
+    ofn.nMaxFile = sizeof(ui.lrc);
+    ofn.lpstrFilter = TEXT("歌词文件(*.txt)\0*.txt\0");
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.hwndOwner = hwndDlg;
+    ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+    GetOpenFileName(&ofn);
+}
 
 void OpenMusic(HWND hwndDlg){
 	OPENFILENAME ofn;
     ZeroMemory(&ofn,sizeof(ofn));
     ofn.lStructSize = sizeof(ofn);
-    ofn.lpstrFile = szFile;//全路径
-    ofn.lpstrFile[0] = TEXT('\0');
-    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFile = ui.file;//全路径
+    ofn.lpstrFile[0] = 0;
+    ofn.nMaxFile = sizeof(ui.file);
     ofn.lpstrFilter = TEXT("音频文件(*.mp3;*.aac)\0*.mp3;*.aac\0");
     ofn.nFilterIndex = 1;
     ofn.lpstrFileTitle = NULL;
@@ -35,33 +57,31 @@ void OpenMusic(HWND hwndDlg){
     ofn.lpstrInitialDir = NULL;
     ofn.hwndOwner = hwndDlg;
     ofn.Flags = OFN_EXPLORER |OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-    GetOpenFileName (&ofn) ;
-    if(szFile[0])
+    GetOpenFileName(&ofn);
+    if(ui.file[0])
     {
-        SetDlgItemText(hwndDlg,EDIT_FILE,szFile);
+        SetDlgItemText(hwndDlg,EDIT_FILE,ui.file);
     }
 }
 
 void MCIOpenMusic(MCIGLOBAL *mci){
 	mci->open.lpstrDeviceType=0;
-	mci->open.lpstrElementName=szFile;
+	mci->open.lpstrElementName=ui.file;
 	mciSendCommand(0,MCI_OPEN,MCI_OPEN_ELEMENT,(DWORD)&mci->open);
-}
-
-void PlayMusic(MCIGLOBAL *mci,int from){
-	//mci->play.dwFrom=from;
-	//mciSendCommand(mci->open.wDeviceID,MCI_PLAY,MCI_FROM,(DWORD)&mci->play);
-	mciSendCommand(mci->open.wDeviceID,MCI_PLAY,0,(DWORD)&mci->play);
 }
 
 void CALLBACK TimerProc(HWND hWnd,UINT nMsg,UINT nTimerid,DWORD dwTime){
 	static int t=0;
-	
+	static char buf[32];
 	//获取音乐时间
 	mci.StatusParms.dwItem=MCI_STATUS_POSITION;
 	mciSendCommand(mci.open.wDeviceID,MCI_STATUS,MCI_WAIT|MCI_STATUS_ITEM,(DWORD)&(mci.StatusParms));
 	//
-	printf("%d\n",mci.StatusParms.dwReturn);
+	#define sec (mci.StatusParms.dwReturn/1000)
+	//printf("%d\n",mci.StatusParms.dwReturn);
+	SendMessage(ui.timeline,TBM_SETPOS,1,sec);
+	sprintf(buf,"%02d:%02d/%02d:%02d\n",sec/60,sec%60,ui.duration/60000,ui.duration/1000%60);
+	SetWindowText(ui.time,buf);
 }
 
 void ListInit(HWND hlist){
@@ -78,7 +98,7 @@ void ListInsert(HWND hlist){
 	ZeroMemory(&lvi,sizeof(lvi));
 	lvi.mask=LVIF_TEXT;
 	lvi.cchTextMax=256;
-	lvi.iItem=0;
+	lvi.iItem=100;
 	lvi.iSubItem=0;
 	lvi.pszText="00";
 	SendMessage(hlist,LVM_INSERTITEM,0,(LPARAM)&lvi);
@@ -89,7 +109,7 @@ void ListInsert(HWND hlist){
 	lvi.pszText="哈哈";
 	SendMessage(hlist,LVM_SETITEM,0,(LPARAM)&lvi);
 	
-	lvi.iItem=1;
+	lvi.iItem=100;
 	lvi.iSubItem=0;
 	lvi.pszText="01";
 	SendMessage(hlist,LVM_INSERTITEM,0,(LPARAM)&lvi);
@@ -107,19 +127,21 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
     case WM_INITDIALOG:
     {
-    	//DWORD dwStyle=GetWindowLong(GetDlgItem(hwndDlg,IDC_LIST1),GWL_STYLE);
-    	//SetWindowLong(GetDlgItem(hwndDlg,IDC_LIST1),GWL_STYLE,dwStyle|LVS_EX_FULLROWSELECT);
-    	//SendDlgItemMessage(hwndDlg,IDC_LIST1,LVM_SETEXTENDEDLISTVIEWSTYLE,0,LVS_EX_FULLROWSELECT);
+    	//获取控件
+    	ui.lrclist =GetDlgItem(hwndDlg,IDC_LIST1);
+    	ui.timeline=GetDlgItem(hwndDlg,SLIDER_MAIN);
+    	ui.time    =GetDlgItem(hwndDlg,IDC_TIME);
+    	
     	ListView_SetExtendedListViewStyle(GetDlgItem(hwndDlg,IDC_LIST1),LVS_EX_FULLROWSELECT|LVS_EX_GRIDLINES);
-    	mci.playing=0;
+    	ListInit(ui.lrclist);
+    	//ListInsert(GetDlgItem(hwndDlg,IDC_LIST1));
     	SetTimer(hwndDlg,114,500,TimerProc);
-    	ListInit(GetDlgItem(hwndDlg,IDC_LIST1));
-    	ListInsert(GetDlgItem(hwndDlg,IDC_LIST1));
     }
     return TRUE;
 
     case WM_CLOSE:
     {
+    	mciSendCommand(mci.open.wDeviceID,MCI_CLOSE,0,0);
         EndDialog(hwndDlg, 0);
     }
     return TRUE;
@@ -134,18 +156,37 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         switch(LOWORD(wParam))
         {
+			case IDM_OPENLRC:
+				OpenLRC(hwndDlg);
+				break;
         	case IDM_MUSIC:
+        		//选择文件
         		OpenMusic(hwndDlg);
+        		//打开音乐
         		MCIOpenMusic(&mci);
+        		//获取音乐长度
+        		mci.StatusParms.dwItem=MCI_STATUS_LENGTH;
+        		mciSendCommand(mci.open.wDeviceID,MCI_STATUS,MCI_WAIT|MCI_STATUS_ITEM,(DWORD)&mci.StatusParms);
+        		ui.duration=mci.StatusParms.dwReturn;
+        		//设置滑块
+        		SendMessage(ui.timeline,TBM_SETRANGE,1,(LPARAM)MAKELONG(0,ui.duration/1000));
 				break;
 			case BTN_PLAY:
-				if(mci.playing){
+				//查询当前状态
+				mci.StatusParms.dwItem=MCI_STATUS_MODE;
+				mciSendCommand(mci.open.wDeviceID,MCI_STATUS,MCI_WAIT|MCI_STATUS_ITEM,(DWORD)&mci.StatusParms);
+				if(mci.StatusParms.dwReturn==MCI_MODE_PLAY){
 					mciSendCommand(mci.open.wDeviceID,MCI_PAUSE,0,(DWORD)&mci.play);
-					mci.playing=0;
+					SetDlgItemText(hwndDlg,BTN_PLAY,">");
 				}else{
-					PlayMusic(&mci,mci.play.dwFrom);
-					mci.playing=1;
+					mciSendCommand(mci.open.wDeviceID,MCI_PLAY,0,(DWORD)&mci.play);
+					SetDlgItemText(hwndDlg,BTN_PLAY,"||");
 				}
+				break;
+			case BTN_STOP:
+				mciSendCommand(mci.open.wDeviceID,MCI_STOP,0,(DWORD)&mci.play);
+				break;
+			default:
 				break;
         }
     }
--- resource.h
+++ resource.h
@@ -4,19 +4,19 @@
 
 #define DLG_MAIN                                100
 #define IDR_MENU1                               101
-#define IDM_OPEN                                40000
 #define SLIDER_MAIN                             40000
 #define BTN_STOP                                40001
-#define IDM_SAVE                                40001
 #define BTN_PLAY                                40002
-#define IDM_EXIT                                40002
 #define IDC_TIME                                40003
-#define IDM_LINEADD                             40003
 #define IDC_LIST1                               40004
-#define IDM_LINEDEL                             40004
-#define IDM_INFO                                40005
 #define BTN_INSERT                              40006
-#define IDM_MUSIC                               40006
 #define BTN_ADD                                 40007
 #define BTN_DEL                                 40008
 #define EDIT_FILE                               40009
+#define IDM_OPENLRC                             50000
+#define IDM_SAVE                                50001
+#define IDM_EXIT                                50002
+#define IDM_LINEADD                             50003
+#define IDM_LINEDEL                             50004
+#define IDM_INFO                                50005
+#define IDM_MUSIC                               50006
